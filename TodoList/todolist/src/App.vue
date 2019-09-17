d957eac0744a926f0f5550e668d20931ae9e53c2
xyq1201 <q2902099330@163.com>
2019-09-17 20:03:21 +0800

    小练习

---
+++ TodoList/todolist/src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <div>
+      <input type="text" placeholder="请输入内容" v-model="newValue"/>
+
+      <button @click="handleSubmit">提交</button>
+    </div>
+     <ul>
+       <!-- 父组件删除事件 -->
+       <todo-item v-for="(item,index) in lists" :key="index" 
+       :content="item" :index="index" @delete="handleDelete(index)"></todo-item>
+     </ul>
+  </div>
+</template>
+<script>
+// 引入
+import listItem from './components/listItem'
+export default {
+  // 映射组件
+  components:{
+    'todo-item':listItem
+  },
+  data(){
+    return{
+      newValue:'',
+      lists:[]
+    }
+  },
+  methods:{
+    handleSubmit(){
+      this.lists.push(this.newValue) 
+      this.newValue = ''
+    },
+    handleDelete(index){
+      this.lists.splice(index,1)
+    }
+  }
+}
+</script>
+<style lang="scss">
+
+</style>
---
+++ TodoList/todolist/src/components/listItem.vue
@@ -0,0 +1,26 @@
+<template>
+<!-- 点击事件 -->
+<li @click="handleInfo">
+{{content}}
+</li>
+
+</template>
+
+<script>
+export default {
+  // 接收父组件的传值
+  props:['content','index'],
+  methods:{
+    handleInfo(){
+      // alert('abc')
+      // 触发父组件自定义delete事件，并传入相应下标
+      this.$emit('delete',this.index)
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
